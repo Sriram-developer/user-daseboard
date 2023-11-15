5f9da9c2823533ef6f50bbee4118bb1a3b6da40e
Sriram <sri761177@gmail.com>
2023-11-15 12:05:01 +0530

    Add tailwind and postscss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      User-Dasboard
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
