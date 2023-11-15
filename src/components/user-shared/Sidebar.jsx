c2fe2d316ce606aeb4eb9666cbdffe15dc1e2bd1
Sriram <sri761177@gmail.com>
2023-11-15 20:25:56 +0530

    Add classnames and mapped the items

--- src/components/user-shared/Sidebar.jsx
+++ src/components/user-shared/Sidebar.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import {FcOk} from 'react-icons/fc'
+import { DASHBOARD_SIDEBAR_LINKS } from '../../lib/consts/navigation'
+import { Link } from 'react-router-dom'
+import classNames from 'classnames'
+
+const linkClasses ='flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base'
+
 
 export default function Sidebar() {
   return (
@@ -8,8 +14,21 @@ export default function Sidebar() {
             <FcOk fontSize={24}/>
             <span className='text-neutral-100 text-lg '>Greenie</span>
         </div>
-        <div className='flex-1'></div>
+        <div className='flex-1'>
+            {DASHBOARD_SIDEBAR_LINKS.map((item) =>(
+                <SidebarLink key={item.Key} item={item}/>
+            ))}
+        </div>
         <div>bottom part</div>
     </div>
   )
+}
+
+function SidebarLink({ item }){
+    return(
+        <Link to={item.path} className={classNames('text-white', linkClasses)}>
+        <span className='text-xl'>{item.icon}</span>
+        {item.label}
+        </Link>
+    )
 }
