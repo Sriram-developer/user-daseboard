a5848de2f664de09a170d85afd8333648b4c6018
Sriram <sri761177@gmail.com>
2023-11-15 19:47:19 +0530

    Adding sidebar bottom link

--- src/lib/consts/navigation.jsx
+++ src/lib/consts/navigation.jsx
@@ -45,3 +45,17 @@ export const DASHBOARD_SIDEBAR_LINKS = [
 	}
 ]
 
+export const DASHBOARD_SIDEBAR_BOTTOM_LINKS = [
+	{
+		key: 'settings',
+		label: 'Settings',
+		path: '/settings',
+		icon: <HiOutlineCog />
+	},
+	{
+		key: 'support',
+		label: 'Help & Support',
+		path: '/support',
+		icon: <HiOutlineQuestionMarkCircle />
+	}
+]
