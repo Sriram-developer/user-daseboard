592bb87a0ce4fca0d8bfb181236f230edfa5c805
Sriram <sri761177@gmail.com>
2023-11-15 21:17:32 +0530

    fixed design for bottom sidebar

--- src/components/user-shared/Sidebar.jsx
+++ src/components/user-shared/Sidebar.jsx
@@ -3,6 +3,7 @@ import {FcOk} from 'react-icons/fc'
 import { DASHBOARD_SIDEBAR_BOTTOM_LINKS, DASHBOARD_SIDEBAR_LINKS } from '../../lib/consts/navigation'
 import { Link, useLocation } from 'react-router-dom'
 import classNames from 'classnames'
+import { HiOutlineLogout } from 'react-icons/hi'
 
 const linkClasses ='flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base'
 
@@ -19,8 +20,15 @@ export default function Sidebar() {
                 <SidebarLink key={item.Key} item={item}/>
             ))}
         </div>
-        <div>
-            {DASHBOARD_SIDEBAR_BOTTOM_LINKS}
+        <div className="flex flex-col gap-0.5 pt-2 border-t border-neutral-700">
+            {DASHBOARD_SIDEBAR_BOTTOM_LINKS.map((item) =>(
+                <SidebarLink key={item.Key} item={item}/>
+            ))}
+
+        <div className={classNames(linkClasses, 'cursor-pointer text-red-500')}>
+        <span className='text-xl'><HiOutlineLogout/></span>
+        Logout
+        </div>
         </div>
     </div>
   )
