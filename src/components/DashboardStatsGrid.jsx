2a188c4053393c39e2ac01caef7cd283182daa7e
Sriram <sri761177@gmail.com>
2023-11-16 15:11:59 +0530

    Added first boxwapper

--- src/components/DashboardStatsGrid.jsx
+++ src/components/DashboardStatsGrid.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react'       
+import { MdFormatListBulleted } from 'react-icons/md'
+
 
 export default function DashboardStatsGrid() {
   return (
     <div className='flex gap-4 w-full'>
-          <BoxWrapper>a</BoxWrapper>
+          <BoxWrapper>
+          <div className="rounded-full h-12 w-12 flex items-center justify-center bg-sky-500">
+	      <MdFormatListBulleted className="text-2xl text-white" />
+				</div>
+				<div className="pl-4">
+					<span className="text-sm text-gray-500 font-light">Total Forms</span>
+					<div className="flex items-center">
+						<strong className="text-xl text-gray-700 font-semibold">#3456</strong>
+						<span className="text-sm text-green-500 pl-2">+343</span>
+					</div>
+				</div>
+          </BoxWrapper>
           <BoxWrapper>b</BoxWrapper>
           <BoxWrapper>c</BoxWrapper>
           <BoxWrapper>d</BoxWrapper>
